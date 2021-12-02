9a2134b5f497385e97015321c3ccdc88ad972973
LenaCScheper <ven.lenysja@ukr.net>
2021-12-02 19:19:50 +0200

    изменения

---
+++ src/fetchCountries.js
@@ -0,0 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './index';
+
+export function fetchCountries(countryName) {
+  const url = `https://restcountries.com/v3.1/name/${countryName}?fields=name,capital,population,flags,languages`;
+
+  if (countryName === '') {
+    Notify.info('Please type country name');
+    refs.countryCardContainer.innerHTML = '';
+    return;
+  }
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      Notify.failure('Sorry, we could not find country for you');
+      refs.countryCardContainer.innerHTML = '';
+
+      throw new Error(response.status);
+    }
+
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import countryCardTmplt from './templates/country-cards.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchCountries } from './fetchCountries';
+
+const refs = {
+  countryCardContainer: document.querySelector('.country-info'),
+  searchBox: document.getElementById('search-box'),
+};
+export { refs };
 
 const DEBOUNCE_DELAY = 300;
+let markup = '';
+
+refs.searchBox.addEventListener('input', debounce(onSearchBoxInput, DEBOUNCE_DELAY));
+
+
+function onSearchBoxInput(e) {
+  const countryName = e.target.value.trim();
+  fetchCountries(countryName).then(renderCountryCard);
+}
+
+function renderCountryCard(country) {
+  markup = countryCardTmplt(country);
+  if (country.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name');
+    refs.countryCardContainer.innerHTML = '';
+    return;
+  } else if (country.length > 1 && country.length <= 10) {
+    listMarkupBuild();
+    return;
+  }
+  cardMarkupBuild();
+}
+
+function listMarkupBuild() {
+  refs.countryCardContainer.innerHTML = markup;
+  const details = document.querySelectorAll('.country-detailed-info');
+  details.forEach(element => element.classList.toggle('is-hidden'));
+}
+
+function cardMarkupBuild() {
+  refs.countryCardContainer.innerHTML = markup;
+  const officialCountryName = document.querySelectorAll('.country-name');
+  officialCountryName.forEach(element =>
+    element.classList.replace('country-name', 'country-title-card'),
+  );
+}
